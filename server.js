4c4ff89be85a9b15bbd2b229b3e5645c261f8397
Kus Maurya <kus.maurya0000@gmail.com>
2021-04-19 23:55:41 +0530

    loading admin app code is written

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+if (process.env.NODE_ENV === "production") {
+  // app.use(express.static("client/build"));
+  // app.get("/", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
+
+  app.use(express.static("admin/build"));
+  app.get("/admin/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "admin", "build", "index.html"));
+  });
+}
+
 app.get("/", (req, res) => {
   res.send("Server is working Fine");
 });
