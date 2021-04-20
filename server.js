885003cf5f9aa8f85abd1106874ada34e2adb541
Kus Maurya <kus.maurya0000@gmail.com>
2021-04-20 07:31:09 +0530

    client app code and script written

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 //to use .env file
 require("dotenv").config();
 
 const app = express();
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("client/build"));
-  // app.get("/", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
+  app.use(express.static("client/build"));
+  app.get("/", (req, res) => {
+    console.log("GET /");
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 
   app.use(express.static("admin/build"));
   app.get("/admin/", (req, res) => {
+    console.log("GET /admin/");
     res.sendFile(path.resolve(__dirname, "admin", "build", "index.html"));
   });
 }
