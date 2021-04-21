1cca0a834699d96688dd334a8574423413d7e8d3
Kus Maurya <kus.maurya0000@gmail.com>
2021-04-21 13:30:17 +0530

    Minor changes

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 const path = require("path");
 
-//to use .env file
-require("dotenv").config();
-
-const app = express();
 app.use(cors());
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("/", (req, res) => {
-    console.log("GET /");
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 
   app.use(express.static("admin/build"));
-  app.get("/admin/", (req, res) => {
-    console.log("GET /admin/");
+  app.get("/admin", (req, res) => {
     res.sendFile(path.resolve(__dirname, "admin", "build", "index.html"));
   });
 }
 
-app.get("/", (req, res) => {
-  res.send("Server is working Fine");
+app.all("/api", (req, res) => {
+  res.status(200).json({
+    message: "API working fine.",
+  });
 });
+app.use("/api/products", require("./routes/public/Product.route.js"));
 
 //Database connection
 const db = require("./database/db");
