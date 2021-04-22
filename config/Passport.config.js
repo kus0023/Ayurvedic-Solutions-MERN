3213f2f4f9d7933f34e643132e730771b9dfc336
Kus Maurya <kus.maurya0000@gmail.com>
2021-04-22 11:31:31 +0530

    local strategy implemented

---
+++ config/Passport.config.js
@@ -0,0 +1,20 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/User");
+
+passport.use(
+  new LocalStrategy(function (email, password, done) {
+    User.findOne({ email: email }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username." });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+      return done(null, user);
+    });
+  })
+);
