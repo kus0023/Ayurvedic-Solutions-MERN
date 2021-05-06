ed9eea67fa1f80fbb3da73e222db69fd0e52bfd6
Kus Maurya <kus.maurya0000@gmail.com>
2021-05-06 19:01:02 +0530

    some fields added

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   description: { type: String },
-  images: { type: String },
-  uses: [String],
-  diseasesCure: [String],
+  image: { type: String },
+  dose: { type: String },
+  benefits: { type: String },
+  disease: { type: mongoose.Schema.Types.ObjectId, ref: "disease" },
   createdAt: { type: Date, default: Date.now },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  modifyAt: { type: Date, default: Date.now },
+  modifyBy: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
 });
 
 const Product = mongoose.model("product", ProductSchema);
-
 module.exports = Product;
