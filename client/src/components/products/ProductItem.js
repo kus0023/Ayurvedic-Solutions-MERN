9b9103a9200b364d93b88cdb9a55b12c3978d20d
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-08 13:29:09 +0530

    first commit

---
+++ client/src/components/products/ProductItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProductItem() {
+  return (
+    <div>
+      <h1>Item 1</h1>
+    </div>
+  );
+}
+
+export default ProductItem;
---
+++ client/src/components/products/ProdutList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ProductItem from "./ProductItem";
+
+class ProdutList extends Component {
+  render() {
+    return (
+      <div>
+        <ProductItem />
+      </div>
+    );
+  }
+}
+
+export default ProdutList;
