a93a99eed94ba195937c9899352581739611f840
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-08 17:35:58 +0530

    ui created withiut animation

--- client/src/components/products/ProductItem.js
+++ client/src/components/products/ProductItem.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Link, useRouteMatch } from "react-router-dom";
 
 function ProductItem() {
+  const { url } = useRouteMatch();
+
   return (
-    <div className="center">
-      <h1>Items </h1>
-    </div>
+    <>
+      <Link to={`${url}yeah`} className="collection-item avatar">
+        <img
+          src="https://images.pexels.com/photos/1003914/pexels-photo-1003914.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt=""
+          className="circle"
+        />
+        <span className="title">Title</span>
+        <p>
+          First Line <br />
+          Second Line
+        </p>
+        <button datatype="link" className="secondary-content">
+          <i className="material-icons">grade</i>
+        </button>
+      </Link>
+    </>
   );
 }
 
