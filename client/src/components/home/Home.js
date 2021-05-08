b6869190cb1ca6f5fe69ef5171043c6ad9cdf560
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-08 11:23:01 +0530

    Added search ui

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Search from "./Search";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="row">
+        <Search />
+      </div>
+      <div className="row"></div>
+    </div>
+  );
 }
 
 export default Home;
