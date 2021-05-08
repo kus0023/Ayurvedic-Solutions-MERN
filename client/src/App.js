5c622bf3957521a58ddd27bc92b0d7a9bbcfe726
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-08 22:18:51 +0530

    store and provider is added

--- client/src/App.js
+++ client/src/App.js
@@ -9,30 +9,36 @@ import ProdutList from "./components/products/ProdutList";
 import DiseaseList from "./components/disease/DiseaseList";
 import ProductDetail from "./components/products/ProductDetail";
 
+//store
+import { Provider } from "react-redux";
+import store from "./redux/Store";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="*">
-          <div>
-            <Navbar />
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/product" exact component={ProdutList} />
-              <Route path="/product/:id" exact component={ProductDetail} />
-              <Route path="/disease" exact component={DiseaseList} />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="*">
+            <div>
+              <Navbar />
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/product" exact component={ProdutList} />
+                <Route path="/product/:id" exact component={ProductDetail} />
+                <Route path="/disease" exact component={DiseaseList} />
 
-              {/* laslty if route is wrong */}
-              <Route path="*">
-                <NotFound />
-              </Route>
-            </Switch>
-          </div>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+                {/* laslty if route is wrong */}
+                <Route path="*">
+                  <NotFound />
+                </Route>
+              </Switch>
+            </div>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
