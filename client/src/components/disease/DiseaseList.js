4f6c6f6247d7c86bdfc80f233f9a03eefdc3ee9e
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-08 13:29:00 +0530

    first commit

---
+++ client/src/components/disease/DiseaseList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class DiseaseList extends Component {
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default DiseaseList;
