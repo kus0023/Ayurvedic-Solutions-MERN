0801eccaa486b7688004c46666d5792bf368ac34
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-08 13:19:39 +0530

    Sidebar added and links updated

--- client/src/components/nav/Navbar.js
+++ client/src/components/nav/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
+import AlwaysShowLinks from "./AlwaysShowLinks";
 import LogedInLinks from "./LogedInLinks";
 import LogedOutLinks from "./LogedOutLinks";
+import Sidebar from "./Sidebar";
 
 function Navbar() {
   return (
@@ -10,12 +12,18 @@ function Navbar() {
           <a href="#!" className="brand-logo">
             Logo
           </a>
+          <a href="#!" data-target="slide-out" className="sidenav-trigger">
+            <i class="material-icons">menu</i>
+          </a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <LogedOutLinks />
             <LogedInLinks />
+            <AlwaysShowLinks />
           </ul>
         </div>
       </nav>
+
+      <Sidebar />
     </div>
   );
 }
