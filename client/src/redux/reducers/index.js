eb014282f754a14dcc7a23cc7caad893f5513bc5
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-09 12:01:47 +0530

    auth reducer added

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
+import AuthReducer from "./Auth.reducer";
 import ProductReducer from "./Product.reducer";
 
 const rootReducer = combineReducers({
+  auth: AuthReducer,
   productState: ProductReducer,
 });
 
