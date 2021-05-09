5eac31a5f285090b1226417d9e6ec6f4c769cc2f
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-09 11:48:24 +0530

    minor fix

--- client/src/components/products/ProdutList.js
+++ client/src/components/products/ProdutList.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 import ProductItem from "./ProductItem";
 import "./css/ProductList.css";
 
+//actions
+import { getProducts } from "../../redux/actions/Product.action";
+import { connect } from "react-redux";
+
 class ProdutList extends Component {
+  componentDidMount() {
+    this.props.getProducts(1, 10);
+  }
   render() {
     return (
       <div className="container section ">
@@ -10,22 +17,22 @@ class ProdutList extends Component {
         <div className="row valign-wrapper">
           <div className="page-box col s4 m3 waves-effect waves-orange">
             <h4 className="flow-text ">
-              <span>Page 1</span>
+              <span>Previous</span>
               <i className="material-icons right rotate-left">double_arrow</i>
             </h4>
           </div>
           <div className="current-page col s4 m6 center-align">
-            <h2 className=" orange-text">2</h2>
+            <h2 className=" orange-text">1</h2>
           </div>
           <div className="page-box col s4 m3 waves-effect waves-orange">
             <h4 className="flow-text">
               <i className="material-icons">double_arrow</i>
-              <span className="right">Page 3</span>
+              <span className="right">Next</span>
             </h4>
           </div>
         </div>
         <div className="row">
-          <div class="collection">
+          <div className="collection">
             <ProductItem />
             <ProductItem />
             <ProductItem />
@@ -37,4 +44,20 @@ class ProdutList extends Component {
   }
 }
 
-export default ProdutList;
+const mapStateToProps = (state) => {
+  const { isLoading, result } = state.productState;
+
+  return {
+    isLoading,
+    pagination: result?.pagination,
+    products: result?.products,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getProducts: (page, limit) => dispatch(getProducts(page, limit)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProdutList);
