6acefebd0accf737160931721b0a4029121c2fea
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-09 18:28:14 +0530

    login is completed

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,81 +1,136 @@
 import React from "react";
 import { CSSTransition } from "react-transition-group";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./Login.css";
 
+import { useForm } from "react-hook-form";
+import { login } from "../../redux/actions/Auth.action";
+import { useDispatch, useSelector } from "react-redux";
+import LinearLoader from "../loader/LinearLoader";
+
 function Login() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm();
+
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+  const history = useHistory();
+
+  const onSubmit = (data) => {
+    const { email, password } = data;
+    dispatch(login(email, password));
   };
+
+  if (auth.user) {
+    history.goBack();
+  }
 
   return (
     <div className="login">
       <div className="row">
         <div className="col s12 m6 offset-m3">
-          <div className="card section">
-            <CSSTransition
-              appear={true}
-              in={true}
-              timeout={600}
-              classNames="title"
-            >
-              <div>
-                <h4 className="section center">Login</h4>
-              </div>
-            </CSSTransition>
-
-            <div className="divider"></div>
-
-            <CSSTransition
-              appear={true}
-              in={true}
-              timeout={600}
-              classNames="form"
-            >
-              <form className="section container" onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="input-field col s12 m6 offset-m3">
-                    <input id="email" type="email" className="validate" />
-                    <label htmlFor="email">Email</label>
-                  </div>
-
-                  <div className="input-field col s12 m6 offset-m3">
-                    <input
-                      autoComplete="false"
-                      id="password"
-                      type="password"
-                      className="validate"
-                    />
-                    <label htmlFor="password">Password</label>
-                  </div>
+          <div className="card ">
+            {auth.login.isLoading && <LinearLoader />}
+            <div className="section">
+              <CSSTransition
+                appear={true}
+                in={true}
+                timeout={600}
+                classNames="title"
+              >
+                <div>
+                  <h4 className="section center">Login</h4>
                 </div>
+              </CSSTransition>
+              {auth.login.error && (
+                <div className=" center red white-text" style={{ padding: 10 }}>
+                  <b>{auth.login.error}</b>
+                </div>
+              )}
 
-                <div className="row">
+              <div className="divider"></div>
+
+              <CSSTransition
+                appear={true}
+                in={true}
+                timeout={600}
+                classNames="form"
+              >
+                <form
+                  className="section container"
+                  onSubmit={handleSubmit(onSubmit)}
+                >
+                  <div className="row">
+                    <div className="input-field col s12 m6 offset-m3">
+                      <input
+                        id="email"
+                        type="email"
+                        className="validate"
+                        required
+                        {...register("email")}
+                      />
+                      <label htmlFor="email">Email</label>
+                      <span
+                        className="helper-text"
+                        data-error="wrong"
+                        data-success="Correct"
+                      >
+                        Enter Email
+                      </span>
+                    </div>
+
+                    <div className="input-field col s12 m6 offset-m3">
+                      <input
+                        autoComplete="false"
+                        id="password"
+                        type="password"
+                        className="validate"
+                        required
+                        {...register("password", { min: 8, required: true })}
+                      />
+                      <label htmlFor="password">Password</label>
+
+                      <span
+                        className="helper-text"
+                        data-error="too short (min: 8)"
+                        data-success="Good"
+                      >
+                        Enter Password
+                      </span>
+                    </div>
+                  </div>
+
+                  <div className="row">
+                    <div className="col s12 m6 offset-m3">
+                      <button
+                        type="submit"
+                        className="waves-effect waves-light btn-small green right"
+                        disabled={auth.login.isLoading}
+                      >
+                        Login
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </CSSTransition>
+              <CSSTransition
+                appear={true}
+                in={true}
+                classNames="link"
+                timeout={1000}
+              >
+                <div className="row center">
                   <div className="col s12 m6 offset-m3">
-                    <button
-                      type="submit"
-                      className="waves-effect waves-light btn-small green right"
-                    >
-                      Login
-                    </button>
+                    Don't have an Account?<Link to="/register"> Register</Link>
+                    <div className="divider"></div>
+                    Go Back To <Link to="/">Home</Link>
                   </div>
                 </div>
-              </form>
-            </CSSTransition>
-            <CSSTransition
-              appear={true}
-              in={true}
-              classNames="link"
-              timeout={1000}
-            >
-              <div className="row center">
-                <div className="col s12 m6 offset-m3">
-                  Don't have an Account?<Link to="/register"> Register</Link>
-                  <div className="divider"></div>
-                  Go Back To <Link to="/">Home</Link>
-                </div>
-              </div>
-            </CSSTransition>
+              </CSSTransition>
+            </div>
           </div>
         </div>
       </div>
