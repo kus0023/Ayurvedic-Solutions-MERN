5c34760e3e2693cb295771f60117f0990e2a2a24
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-09 18:28:00 +0530

    linear loader for login and register

---
+++ client/src/components/loader/LinearLoader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LinearLoader() {
+  return (
+    <div className="progress">
+      <div className="indeterminate"></div>
+    </div>
+  );
+}
+
+export default LinearLoader;
