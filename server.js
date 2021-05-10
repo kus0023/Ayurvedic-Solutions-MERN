d2f8aefede3417148c4c8afb0b180df43744b859
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-10 12:28:04 +0530

    static file route updated. refreshing of page can be done.

--- server.js
+++ server.js
@@ -13,20 +13,22 @@ app.use(express.urlencoded({ extended: false }));
 // Database connection
 require("./database/db");
 
+//All route is in one folder related to api
+app.use("/api", require("./routes"));
+
 //Heroku environment settings
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("/", (req, res) => {
+  app.use(express.static("admin/build"));
+  app.get("/admin*", (req, res) => {
+    return res.sendFile(
+      path.resolve(__dirname, "admin", "build", "index.html")
+    );
+  });
+
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-
-  app.use(express.static("admin/build"));
-  app.get("/admin", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "admin", "build", "index.html"));
-  });
 }
-
-//All route is in one folder related to api
-app.use("/api", require("./routes"));
 
 module.exports = app;
