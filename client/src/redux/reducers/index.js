0616c13c7b39f3d319a8ce8f7437e3afb4f37de0
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-10 18:35:10 +0530

    searchReducer added for home searching

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import AuthReducer from "./Auth.reducer";
 import ProductReducer from "./Product.reducer";
+import SearchReducer from "./Search.reducer";
 
 const rootReducer = combineReducers({
   auth: AuthReducer,
   productState: ProductReducer,
+  searchState: SearchReducer,
 });
 
 export default rootReducer;
