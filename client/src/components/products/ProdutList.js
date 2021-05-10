054a3c0df1709be9493b2145d52f3fee80acc90b
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-10 15:33:37 +0530

    completed with animations

--- client/src/components/products/ProdutList.js
+++ client/src/components/products/ProdutList.js
@@ -5,26 +5,57 @@ import "./css/ProductList.css";
 //actions
 import { getProducts } from "../../redux/actions/Product.action";
 import { connect } from "react-redux";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { SwitchTransition } from "react-transition-group";
 
+const limit = 3;
 class ProdutList extends Component {
   componentDidMount() {
-    this.props.getProducts(1, 10);
+    this.props.getProducts(1, limit);
   }
+
+  handleNextPage = () => {
+    const { pagination } = this.props;
+
+    if (pagination?.next) {
+      this.props.getProducts(pagination.next.page, limit);
+    }
+  };
+
+  handlePrevPage = () => {
+    const { pagination } = this.props;
+    if (pagination?.prev) {
+      this.props.getProducts(pagination.prev.page, limit);
+    }
+  };
+
   render() {
+    const { pagination } = this.props;
+    if (!pagination) {
+      return <h1>Loading</h1>;
+    }
     return (
       <div className="container section ">
         {/* pagination */}
-        <div className="row valign-wrapper">
-          <div className="page-box col s4 m3 waves-effect waves-orange">
+        <div className="row valign-wrapper center">
+          <div
+            className="page-box col s4 m3 btn white black-text waves-effect waves-orange "
+            disabled={pagination?.prev ? false : true}
+            onClick={this.handlePrevPage}
+          >
             <h4 className="flow-text ">
-              <span>Previous</span>
-              <i className="material-icons right rotate-left">double_arrow</i>
+              <span className="left">Prev</span>
+              <i className="material-icons rotate-left">double_arrow</i>
             </h4>
           </div>
-          <div className="current-page col s4 m6 center-align">
-            <h2 className=" orange-text">1</h2>
+          <div className="current-page col s4 m6 center-align ">
+            <h2 className=" orange-text">{pagination?.current}</h2>
           </div>
-          <div className="page-box col s4 m3 waves-effect waves-orange">
+          <div
+            className="page-box col s4 m3 waves-effect waves-orange btn white black-text"
+            disabled={pagination?.next ? false : true}
+            onClick={this.handleNextPage}
+          >
             <h4 className="flow-text">
               <i className="material-icons">double_arrow</i>
               <span className="right">Next</span>
@@ -32,12 +63,22 @@ class ProdutList extends Component {
           </div>
         </div>
         <div className="row">
-          <div className="collection">
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-          </div>
+          <TransitionGroup className="collection">
+            {this.props.products?.map((product, i) => (
+              <SwitchTransition key={i}>
+                <CSSTransition
+                  appear={true}
+                  key={product._id}
+                  timeout={500}
+                  classNames="product-item"
+                >
+                  <div>
+                    <ProductItem product={product} />
+                  </div>
+                </CSSTransition>
+              </SwitchTransition>
+            ))}
+          </TransitionGroup>
         </div>
       </div>
     );
