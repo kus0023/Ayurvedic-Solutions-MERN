676e9e0a516879eb55fa717c899159c22ce06ad9
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-10 20:52:29 +0530

    link is updated and also name is loading dynamically

--- client/src/components/home/SearchResultItem.js
+++ client/src/components/home/SearchResultItem.js
@@ -1,23 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-function SearchResultItem() {
+function SearchResultItem({ item }) {
+  const searchState = useSelector((state) => state.searchState);
   return (
     <>
-      <Link to="" className="collection-item avatar">
-        <img
-          src="https://images.pexels.com/photos/1003914/pexels-photo-1003914.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-          alt=""
-          className="circle"
-        />
-        <span className="title">Title</span>
-        <p>
-          First Line <br />
-          Second Line
+      <Link to={`${searchState.type}/${item._id}`} className="collection-item ">
+        <span className="title">{item.name}</span>
+        <p>{item.description}</p>
+        <p datatype="link" className="secondary-content">
+          {item.createdAt}
         </p>
-        <button datatype="link" className="secondary-content">
-          <i className="material-icons">grade</i>
-        </button>
       </Link>
     </>
   );
