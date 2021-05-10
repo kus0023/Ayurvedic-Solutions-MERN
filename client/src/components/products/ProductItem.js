5f028d1dff22c83b90e358b20939030f5ec2ef0d
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-10 15:33:12 +0530

    UI is populated with data

--- client/src/components/products/ProductItem.js
+++ client/src/components/products/ProductItem.js
@@ -1,26 +1,21 @@
 import React from "react";
-import { Link, useRouteMatch, useParams } from "react-router-dom";
+import { Link, useRouteMatch } from "react-router-dom";
 
-function ProductItem() {
+function ProductItem({ product }) {
   const { url } = useRouteMatch();
-  const { id } = useParams();
 
   return (
     <>
-      <Link to={`${url}/${id}`} className="collection-item avatar">
-        <img
-          src="https://images.pexels.com/photos/1003914/pexels-photo-1003914.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-          alt=""
-          className="circle"
-        />
-        <span className="title">Title</span>
-        <p>
-          First Line <br />
-          Second Line
+      <Link
+        to={`${url}/${product._id}`}
+        className="collection-item avatar black-text"
+      >
+        <img src={product.image} alt={product.name} className="circle" />
+        <span className="title">{product.name}</span>
+        <p>{product.description}</p>
+        <p datatype="link" className="secondary-content">
+          {/* <i className="material-icons">grade</i> */}createdAt
         </p>
-        <button datatype="link" className="secondary-content">
-          <i className="material-icons">grade</i>
-        </button>
       </Link>
     </>
   );
