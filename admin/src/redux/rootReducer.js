d8fcdf9af579d31e417faccb342a191f80124a9a
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-13 11:17:51 +0530

    authentication page added

--- admin/src/redux/rootReducer.js
+++ admin/src/redux/rootReducer.js
@@ -1,10 +1,12 @@
-import { combineReducers } from 'redux'
-import userReducer from './users/userReducer'
-import productReducer from './products/productReducer'
+import { combineReducers } from "redux";
+import userReducer from "./users/userReducer";
+import productReducer from "./products/productReducer";
+import AuthenticationReducer from "./dashboard/AuthenticationReducer";
 
-const rootReducer=combineReducers({
-    auth:userReducer,
-    product:productReducer
-})
+const rootReducer = combineReducers({
+  auth: userReducer,
+  product: productReducer,
+  authenticationPage: AuthenticationReducer,
+});
 
-export default rootReducer
+export default rootReducer;
