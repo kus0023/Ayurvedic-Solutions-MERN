4d1f5906178ad73b51afd8056892eb358018526d
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2021-05-15 12:00:01 +0530

    UI populated with data

--- client/src/components/cart/CartItem.js
+++ client/src/components/cart/CartItem.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function CartItem({ item, handleDelete }) {
+  const { product } = item;
   return (
     <div className="row collection-item">
-      <div className="col s10 m8 ">
-        <h6 className="truncate">{item._id}</h6>
+      <div className="col s12 m2">
+        <img
+          className="responsive-img"
+          alt={product?.name}
+          src={product?.image}
+        />
       </div>
-      <div className="col right">
+      <Link to={`/product/${product._id}`}>
+        <div className="col s8 m8">
+          <h6 className="truncate">{product.name}</h6>
+        </div>
+      </Link>
+
+      <div className="col s4 m2 ">
         <button
-          className="btn wave-effect red "
+          className="btn wave-effect red right "
           onClick={() => handleDelete(item._id)}
         >
           <i className="material-icons ">close</i>
